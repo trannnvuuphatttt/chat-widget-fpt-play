62a0deb44f8bbf600fe9dae33191a1503995c77a
KallaneKoe <namnguyenhoang0903@gmail.com>
2024-10-07 10:07:45 +0700

    commit

--- plugins/widget.js
+++
@@ -1,6 +0,0 @@
-import { defineCustomElement } from "vue";
-import Widget from "../components/Widget.vue";
-
-const WidgetElement = defineCustomElement(Widget);
-
-customElements.define("widget-component", WidgetElement);
