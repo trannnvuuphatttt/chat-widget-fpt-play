0b58e91808ce06d2deaa1351a3de9a07f0ce1408
Truong Nhu Tuan <tuantn22@fpt.com>
2024-11-05 13:31:43 +0700

    update code

--- components/widget-components/chat-bubble-components/bubble-message.vue
+++ components/widget-components/chat-bubble-components/bubble-message.vue
@@ -1,13 +1,23 @@
 <template>
-    <div v-if="messageLoading" class="ml-[6px] font-sf-pro-display flex flex-row gap-3 w-[86] h-[54]">
-        <div class="w-2 h-2 rounded-full bg-[#949494] animate-pulse"></div>
-        <div class="w-2 h-2 rounded-full  bg-[#949494] animate-pulse "></div>
-        <div class="w-2 h-2 rounded-full  bg-[#949494] animate-pulse "></div>
-    </div>
-    <div v-else class="font-sf-pro-display">{{ props.message }}</div>
+  <div
+    v-if="messageLoading"
+    class="ml-2 font-sf-pro-display flex flex-row gap-2 w-fit h-fit"
+  >
+    <div class="w-2 h-2 rounded-full bg-gray-500 animate-pulse"></div>
+    <div class="w-2 h-2 rounded-full bg-gray-500 animate-pulse"></div>
+    <div class="w-2 h-2 rounded-full bg-gray-500 animate-pulse"></div>
+  </div>
+  <div
+    v-else
+    class="font-sf-pro-display text-sm md:text-base break-words w-fit max-w-full"
+  >
+    {{ props.message }}
+  </div>
 </template>
+
 <script lang="js" setup>
 import { useMessage } from "~/stores/messages";
+const { ref, onMounted, defineProps } = Vue;
 const props = defineProps({
     message: String,
 });
