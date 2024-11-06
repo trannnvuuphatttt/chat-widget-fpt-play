977f03f8a198eef7f74c1d9a46e59a953c64ea86
trannnvuuphat <tranvuphato@gmail.com>
2024-11-06 13:48:14 +0700

    testtestresponsive 11/6

--- components/widget-components/chat-bubble-components/bot-chat-bubble.vue
+++ components/widget-components/chat-bubble-components/bot-chat-bubble.vue
@@ -1,21 +1,15 @@
 <template class="relative">
   <div>
     <div class="flex items-center">
-      <img
-        src="assets/images/avatar.png"
-        class="h-6 w-6 inline-block mb-2 mr-1"
-      />
+      <img src="assets/images/avatar.png" class="h-6 w-6 inline-block mb-2 mr-1" />
       <div class="text-sm inline-block mb-2 ml-1">FPT Play AI Support</div>
     </div>
-    <div
-      class="bg-white text-md mb-2 h-14 p-4 text-base w-full sm:w-84 rounded-tl rounded-r-2xl rounded-b-2xl"
-      v-if="
-        messageStore.isLoading &&
-        flag &&
-        Array.isArray(displayMessage) &&
-        props.message.length <= 1
-      "
-    >
+    <div class="bg-white text-md mb-2 h-14 p-4 text-base w-full sm:w-84 rounded-tl rounded-r-2xl rounded-b-2xl" v-if="
+      messageStore.isLoading &&
+      flag &&
+      Array.isArray(displayMessage) &&
+      props.message.length <= 1
+    ">
       <div class="ml-1 mt-1.5 flex flex-row gap-3">
         <div class="w-2 h-2 rounded-full bg-gray-600 animate-pulse"></div>
         <div class="w-2 h-2 rounded-full bg-gray-600 animate-pulse"></div>
@@ -23,86 +17,51 @@
       </div>
     </div>
     <div v-else>
-      <div
-        v-if="
-          (displayMessage && receiveMessage.length >= 1) ||
-          displayMessage?.length > 1
-        "
-      >
+      <div v-if="
+        (displayMessage && receiveMessage.length >= 1) ||
+        displayMessage?.length > 1
+      ">
         <div
           class="bg-white rounded-tl rounded-r-2xl rounded-b-2xl text-md mb-2 h-fit p-4 text-base font-sf-pro-display"
-          v-for="(item, index) in displayMessage"
-          :key="index"
-        >
+          v-for="(item, index) in displayMessage" :key="index">
           <BubbleMessage :message="item"></BubbleMessage>
         </div>
       </div>
-      <div
-        v-else
-        class="bg-white rounded-tl rounded-r-2xl rounded-b-2xl text-md mb-2 h-fit p-4 text-base font-sf-pro-display"
-      >
+      <div v-else
+        class="bg-white rounded-tl rounded-r-2xl rounded-b-2xl text-md mb-2 h-fit p-4 text-base font-sf-pro-display">
         <div v-html="marked(displayMessage[0])"></div>
       </div>
 
-      <div
-        class="bg-white rounded-tl-sm rounded-r-lg rounded-b-lg text-md mb-2 h-fit p-4 text-base"
-        v-if="Array.isArray(props.urls)"
-        v-for="(item, index) in props.urls"
-        :key="index"
-      >
+      <div class="bg-white rounded-tl-sm rounded-r-lg rounded-b-lg text-md mb-2 h-fit p-4 text-base"
+        v-if="Array.isArray(props.urls)" v-for="(item, index) in props.urls" :key="index">
         <a :href="item" target="_blank" class="underline">{{ item }}</a>
       </div>
     </div>
     <span class="flex flex-row justify-between flex-wrap h-fit">
-      <p
-        class="text-xs text-gray-400"
-        v-if="props.timeStamp !== 'NaN ngày trước'"
-      >
+      <p class="text-xs text-gray-400" v-if="props.timeStamp !== 'NaN ngày trước'">
         {{ props.timeStamp }}
       </p>
-      <div
-        v-if="
-          props.flag &&
-          modalStore.isChatting &&
-          props.timeStamp !== 'NaN ngày trước'
-        "
-        class="flex space-x-2"
-      >
+      <div v-if="
+        props.flag &&
+        modalStore.isChatting &&
+        props.timeStamp !== 'NaN ngày trước'
+      " class="flex w-[48px] space-x-2">
         <button class="cursor-pointer" @click="Like()">
-          <img
-            v-if="!reviewStateLike"
-            class="w-4 h-4"
-            src="/assets/images/like.png"
-            alt="Like"
-          />
-          <img
-            v-else
-            class="w-4 h-4"
-            src="/assets/images/like_orange.png"
-            alt="Liked"
-          />
+          <img v-if="!reviewStateLike" class="w-4 h-4" src="/assets/images/like.png" alt="Like" />
+          <img v-else class="w-4 h-4" src="/assets/images/like_orange.png" alt="Liked" />
         </button>
-        <button class="cursor-pointer" @click="Dislike()">
-          <img
-            v-if="!reviewStateDislike"
-            class="w-4 h-4"
-            src="/assets/images/dislike.png"
-            alt="Dislike"
-          />
-          <img
-            v-else
-            class="w-4 h-4"
-            src="/assets/images/dislike_orange.png"
-            alt="Disliked"
-          />
+        <button class="cursor-pointer right-0" @click="Dislike()">
+          <img v-if="!reviewStateDislike" class="w-4 h-4" src="/assets/images/dislike.png" alt="Dislike" />
+          <img v-else class="w-4 h-4" src="/assets/images/dislike_orange.png" alt="Disliked" />
         </button>
       </div>
     </span>
   </div>
+
 </template>
 
 <script lang="js" setup>
-import { onMounted, ref, watch } from "vue";
+import { onMounted, ref, resolveComponent, watch } from "vue";
 import BubbleMessage from "./bubble-message.vue";
 import { useModalStore } from "~/stores/modal";
 import { useSnackBarStore } from "~/stores/snackbar";
@@ -194,35 +153,47 @@ onMounted(() => {
 /* Giảm kích thước font chữ cho màn hình dưới 400px */
 @media (max-width: 400px) {
   .text-base {
-    font-size: 14px; /* Điều chỉnh theo nhu cầu */
+    font-size: 14px;
+    /* Điều chỉnh theo nhu cầu */
   }
+
   .text-sm {
     font-size: 12px;
   }
+
   .text-md {
     font-size: 13px;
   }
+
   .text-xs {
     font-size: 10px;
   }
+
   .h-6 {
     height: 20px;
   }
+
   .w-6 {
     width: 20px;
   }
+
   .h-14 {
-    height: auto; /* Điều chỉnh nếu cần */
+    height: auto;
+    /* Điều chỉnh nếu cần */
   }
+
   .p-4 {
     padding: 12px;
   }
+
   .w-4 {
     width: 16px;
   }
+
   .h-4 {
     height: 16px;
   }
+
   .mb-2 {
     margin-bottom: 8px;
   }
