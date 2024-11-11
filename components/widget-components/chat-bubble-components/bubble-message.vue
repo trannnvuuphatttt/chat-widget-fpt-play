a88927e694be81ecfb99772bec52f1e20c264a7d
Truong Nhu Tuan <tuantn22@fpt.com>
2024-11-11 17:42:40 +0700

    update css

--- components/widget-components/chat-bubble-components/bubble-message.vue
+++ components/widget-components/chat-bubble-components/bubble-message.vue
@@ -6,7 +6,7 @@
     <div class="w-1 h-1 ml-[4px] mt-[26px] rounded-full bg-gray-600 animate-pulse"></div>
   </div>
   <div v-else
-    class="md:text-base break-words max-w-full bg-white rounded-tl rounded-r-2xl rounded-b-2xl text-md mb-2 h-fit p-4 text-base font-sf-pro-display w-[338px] sm:w-[420px] lg:w-[338px] xl:w[338px] ml-4">
+    class="md:text-base break-words max-w-full bg-white rounded-tl rounded-r-2xl rounded-b-2xl text-md mb-2 h-fit p-4 text-base font-sf-pro-display w-[338px] sm:w-[420px] md:w-[480px] lg:w-[338px] xl:w-[338px] ml-4">
     {{ props.message }}
   </div>
 </template>
